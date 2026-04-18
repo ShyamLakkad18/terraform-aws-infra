5089633ceece1d223aa5d7a1144ded1b0b494858
ShyamLakkad18 <devops1845@gmail.com>
2026-04-18 12:19:22 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,35 @@
+variable "env" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "ami-id" {
+  type = string
+}
+
+variable "volume_size" {
+  type = number
+}
+
+variable "volume_type" {
+  type = string
+}
+
+variable "name" {
+  type = string
+}
+
+variable "count-id" {
+  type = number
+}
+
+variable "bucket-name" {
+  type = string
+}
+
+variable "region-name" {
+  type = string
+}
