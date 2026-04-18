150ddb1f54dfb92d5a7f29962d140de3b40ecb7f
ShyamLakkad18 <devops1845@gmail.com>
2026-04-18 12:18:06 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+module "dev-infra" {
+ source = "./plan"
+
+ env = "dev"
+ instance_type = "t3.micro"
+ ami-id = "ami-0e12ffc2dd465f6e4"
+ volume_size = 15
+ volume_type = "gp3"
+ name = "terraform instance"
+ count-id = 3
+ bucket-name = "terrafomr-automate-infra-bucket"
+ region-name = "ap-south-1"
+
+}
