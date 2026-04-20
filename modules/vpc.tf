7fa725fa20964f07bf687d3beef332bce91f332e
ShyamLakkad18 <devops1845@gmail.com>
2026-04-20 08:10:33 +0530

    Create vpc.tf

---
+++ modules/vpc.tf
@@ -0,0 +1,40 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "my-vpc"
+  }
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "my-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+}
+
+resource "aws_route_table" "my_rt" {
+  vpc_id = aws_vpc.my_vpc.id
+}
+
+resource "aws_route" "my_route" {
+  route_table_id         = aws_route_table.my_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.my_igw.id
+}
+
+# Route Table Association
+resource "aws_route_table_association" "my_assoc" {
+  subnet_id      = aws_subnet.my_subnet.id
+  route_table_id = aws_route_table.my_rt.id
+}
