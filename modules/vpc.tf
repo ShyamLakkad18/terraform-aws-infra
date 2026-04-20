841bf17fba1094f2f75afd76b84cf28aa020d42c
ShyamLakkad18 <devops1845@gmail.com>
2026-04-20 08:21:40 +0530

    Update vpc.tf

--- modules/vpc.tf
+++ modules/vpc.tf
@@ -1,39 +1,22 @@
 provider "aws" {
-  region = var.region-name
+  region = "ap-south-1"
 }
 
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/16"
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.0.0"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["ap-south-1a", "ap-south-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
 
   tags = {
-    Name = var.vpc-name
+    Terraform   = "true"
+    Environment = "dev"
   }
-}
-
-resource "aws_subnet" "my_subnet" {
-  vpc_id     = aws_vpc.my_vpc.id
-  cidr_block = "10.0.1.0/24"
-
-  tags = {
-    Name = var.my-subnet
-  }
-}
-
-resource "aws_internet_gateway" "my_igw" {
-  vpc_id = aws_vpc.my_vpc.id
-}
-
-resource "aws_route_table" "my_rt" {
-  vpc_id = aws_vpc.my_vpc.id
-}
-
-resource "aws_route" "my_route" {
-  route_table_id         = aws_route_table.my_rt.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.my_igw.id
-}
-
-resource "aws_route_table_association" "my_assoc" {
-  subnet_id      = aws_subnet.my_subnet.id
-  route_table_id = aws_route_table.my_rt.id
 }
